b0348a5a16b9a57ed93c05738aaf0ae294eca09e
Amal K K <kkamalkk1994@gmail.com>
2022-07-31 12:33:17 +0530

    Error Boundary case

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import MathAdd from "./MathAdd";
 import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback } from "../Utils/ErrorFallBackUtil";
 
 const ImageContainer = React.lazy(() => import("./ImageContainer"));
 
@@ -8,10 +9,17 @@ const Container = () => {
   return (
     <div>
       <h2>Hello from the container!</h2>
-      <MathAdd />
-      <Suspense fallback={<div>Loading...</div>}>
-        <ImageContainer />
-      </Suspense>
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          window.location.reload();
+        }}
+      >
+        <MathAdd />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ImageContainer />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
