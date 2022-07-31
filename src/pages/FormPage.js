69d41378d50ecf647f717ae4fad27ab77ba3e10e
Amal K K <kkamalkk1994@gmail.com>
2022-07-31 19:51:21 +0530

    submit flow

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -1,83 +1,101 @@
-import { useState } from "react";
-import Styles from "./FormPage.module.css";
+import { useState } from "react"
+import Styles from "./FormPage.module.css"
 
 const FormPage = () => {
-  const initialValues = { title: "", body: "", userId: "" };
-  const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const initialValues = { title: "", body: "", userId: "" }
+  const [formValues, setFormValues] = useState(initialValues)
+  const [formErrors, setFormErrors] = useState({})
+  const [isSubmitted, setIsSubmitted] = useState(false)
 
   const handleOnChange = (e) => {
     //console.log(e.target.value);
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
+    const { name, value } = e.target
+    setFormValues({ ...formValues, [name]: value })
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormErrors(validateFields(formValues));
+    e.preventDefault()
+    setFormErrors(validateFields(formValues))
     if (!!formErrors) {
-      setIsSubmitted(true);
+      setIsSubmitted(true)
+      console.log("isSubmitted", isSubmitted)
     }
-  };
+  }
 
   const validateFields = (values) => {
-    const errors = {};
+    const errors = {}
     if (!values.title) {
-      errors.title = "Title is required!";
+      errors.title = "Title is required!"
     }
     if (!values.body) {
-      errors.body = "Body is required!";
+      errors.body = "Body is required!"
     }
     if (!values.userId) {
-      errors.userId = "user id is required!";
+      errors.userId = "user id is required!"
     }
 
-    return errors;
-  };
+    return errors
+  }
 
   return (
     <div className={Styles.formContainer}>
-      <h2>Enter the details</h2>
-      <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-      <form onSubmit={handleSubmit}>
-        <div className={Styles.formField}>
-          <label>Title</label>
-          <input
-            type="text"
-            name="title"
-            placeholder="title"
-            value={formValues.title}
-            onChange={handleOnChange}
-          />
-          <p>{formErrors.title}</p>
-        </div>
-        <div className={Styles.formField}>
-          <label>body</label>
-          <input
-            type="text"
-            name="body"
-            placeholder="body"
-            value={formValues.body}
-            onChange={handleOnChange}
-          />
-          <p>{formErrors.body}</p>
-        </div>
-        <div className={Styles.formField}>
-          <label>user id</label>
-          <input
-            type="text"
-            name="userId"
-            placeholder="user id"
-            value={formValues.userId}
-            onChange={handleOnChange}
-          />
-          <p>{formErrors.userId}</p>
-        </div>
-        <button className={Styles.submitButton}>Submit</button>
-      </form>
+      <p
+        className={
+          Object.keys(formErrors).length === 0 && isSubmitted
+            ? Styles.formContainer
+            : Styles.noDisplay
+        }
+      >
+        Submitted successfully!!!
+      </p>
+      <div
+        className={
+          Object.keys(formErrors).length === 0 && isSubmitted
+            ? Styles.noDisplay
+            : Styles.formContainer
+        }
+      >
+        <h2>Enter the details</h2>
+        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+        <form onSubmit={handleSubmit}>
+          <div className={Styles.formField}>
+            <label>Title</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="title"
+              value={formValues.title}
+              onChange={handleOnChange}
+            />
+            <p>{formErrors.title}</p>
+          </div>
+          <div className={Styles.formField}>
+            <label>body</label>
+            <input
+              type="text"
+              name="body"
+              placeholder="body"
+              value={formValues.body}
+              onChange={handleOnChange}
+            />
+            <p>{formErrors.body}</p>
+          </div>
+          <div className={Styles.formField}>
+            <label>user id</label>
+            <input
+              type="text"
+              name="userId"
+              placeholder="user id"
+              value={formValues.userId}
+              onChange={handleOnChange}
+            />
+            <p>{formErrors.userId}</p>
+          </div>
+          <button className={Styles.submitButton}>Submit</button>
+        </form>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default FormPage;
+export default FormPage
